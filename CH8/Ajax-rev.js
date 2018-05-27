6df72c8e3cf6ce6efd872b4aa403d9755a96ee93
gittesthln <gittest@google.com>
2018-05-27 14:26:11 +0900

    add a section on server

--- CH8/Ajax-rev.js
+++ CH8/Ajax-rev.js
@@ -1,17 +1,15 @@
-let httpObj;
 function getData() {//// ref="OpenS"
-  httpObj = new XMLHttpRequest();//// ref="MakeObj"
+  let httpObj = new XMLHttpRequest();//// ref="MakeObj"
   if(httpObj) {//// ref="Succeed"
-	  httpObj.onreadystatechange = function () {//// ref="GetS"
+    httpObj.onreadystatechange = function () {//// ref="GetS"
       if(httpObj.readyState == 4 && httpObj.status == 200) {//// ref="requestOK"
         alert(httpObj.responseText);//// ref="showSendText"
         let polygon = document.getElementById("npolygon");//// ref="setDS"
         polygon.setAttribute("points", httpObj.responseText);//// ref="setDE"
       }
-		}
-	}
-  httpObj.open("GET","./svg-polygon-ajax.php?N=" +//// ref="MakeReqS"
-    encodeURI(document.getElementById("N").value),true);//// ref="MakeReqE"
-  httpObj.send(null);//// ref="sendnull"
+    }//// ref="GetE"
+    httpObj.open("GET","./svg-polygon-ajax.php?N=" +//// ref="MakeReqS"
+      encodeURI(document.getElementById("N").value),true);//// ref="MakeReqE"
+    httpObj.send(null);//// ref="sendnull"
+  }
 }//// ref="OpenE"
-//// ref="GetE"
